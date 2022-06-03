8413ed39f41a82f9aee509dfcc2a085833857b31
woarthur <78649056+woarthur@users.noreply.github.com>
2022-06-03 21:09:16 +0800

    Add Time class

---
+++ src/main/java/newrender/core/Time.java
@@ -0,0 +1,12 @@
+package newrender.core;
+
+public class Time
+{
+    private static final long SECOND = 1000000000L;
+
+    public static double GetTime()
+    {
+        return (double)System.nanoTime()/(double)SECOND;
+    }
+}
+
